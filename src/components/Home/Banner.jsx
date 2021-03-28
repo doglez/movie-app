3838507206283967e3059f345d62d7d2b6650c0c
Danilo Gonzalez Ardon <danilo.j.gonzalez@gmail.com>
2021-03-27 22:40:13 -0600

    instalacion de axios-hooks

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -1,17 +1,28 @@
+import useAxios from 'axios-hooks'
 import React, { useEffect, useState } from 'react'
 import {ApiRequest} from '../Request/ApiRequest'
 
 const Banner = () => {
 
-    const [movie, setMovie] = useState([])
+    const [{data, loading, error}] = useAxios({
+        method: 'GET',
+        url: ApiRequest.trending
+    })
+
+    const [movies, setMovies] = useState([])
     
     useEffect(() => {
-        
-    }, [])
+        if (data && data.results) {
+            setMovies(data.results)
+        } else if (error) {
+            setMovies(error.message)
+        }        
+    }, [data, error])
 
     return (
         <div>
-            
+            <p>{loading && 'Loading...'}</p>
+            {console.log(movies)}
         </div>
     )
 }
